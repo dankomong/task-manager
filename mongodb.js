ef14d88057a073a192d9157718f3c8041d89205c
Daniel Ko <dankomong@gmail.com>
2019-08-28 20:01:55 -0400

    initialize connection to mongodb

---
+++ mongodb.js
@@ -0,0 +1,27 @@
+// CRUD
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const database = 'task-manager';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    return console.log('Unable to connect to database');
+  }
+
+  const db = client.db(database);
+
+  db.collection('users').insertOne({
+    name: 'Daniel',
+    age: 23
+  }, (error, result) => {
+    if (error) {
+      return console.log('Unable to insert user.');
+    }
+
+    console.log(result.ops);
+  })
+
+});
